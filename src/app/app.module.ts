fc3ce32cca383d7664232cffe9656fa60b746da2
Edwin <edwinbayona@wposs.com>
2023-12-08 20:32:53 -0500

    Base de proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './shared/layouts/card/card.component';
+import { LandingPageComponent } from './shared/layouts/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
